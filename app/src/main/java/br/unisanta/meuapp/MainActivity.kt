e3cee971fe94159813ed6043dca4d4d7e0c429c8
Lucas Barros Simon <lucasweacked21@gmail.com>
2026-08-15 06:58:02 -0300

    feat: adicionei as coisas da ultima aula.

--- app/src/main/java/br/unisanta/meuapp/MainActivity.kt
+++ app/src/main/java/br/unisanta/meuapp/MainActivity.kt
@@ -1,20 +1,30 @@
 package br.unisanta.meuapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val edtValor1 = findViewById<EditText>(R.id.edt_valor1)
+        val edtValor2 = findViewById<EditText>(R.id.edt_valor2)
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+        val txvResultado = findViewById<TextView>(R.id.txv_resultado)
+
+        btnCalcular.setOnClickListener {
+            val valor1 = edtValor1.text.toString().toDouble();
+            val valor2 = edtValor2.text.toString().toDouble();
+            val soma = valor1 + valor2;
+
+            txvResultado.setText("A soma é: $soma");
+            edtValor1.text.clear();
+            edtValor2.text.clear();
         }
     }
 }
