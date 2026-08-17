f5b5d9a8a85aa4ee00514bc1731888cf4420f8ee
Lucas Barros Simon <lucasweacked21@gmail.com>
2026-08-16 22:34:56 -0300

    feat: terminei o tp1.1

--- app/src/main/java/br/unisanta/meuapp/MainActivity.kt
+++ app/src/main/java/br/unisanta/meuapp/MainActivity.kt
@@ -5,26 +5,25 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val edtNome = findViewById<EditText>(R.id.edt_nome)
-        val edtIdade = findViewById<EditText>(R.id.edt_idade)
+        val edtValor1 = findViewById<EditText>(R.id.edt_valor1)
+        val edtValor2 = findViewById<EditText>(R.id.edt_valor2)
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
         val txvResultado = findViewById<TextView>(R.id.txv_resultado)
 
         btnCalcular.setOnClickListener {
-            val nome = edtNome.text.toString()
-            val idade = edtIdade.text.toString()
+            val valor1 = edtValor1.text.toString().toDouble();
+            val valor2 = edtValor2.text.toString().toDouble();
+            val soma = valor1 + valor2;
 
-            txvResultado.text = "Olá, $nome! Você tem $idade anos."
-
-            edtNome.text.clear()
-            edtIdade.text.clear()
+            txvResultado.setText("A soma é: $soma");
+            edtValor1.text.clear();
+            edtValor2.text.clear();
         }
     }
 }
