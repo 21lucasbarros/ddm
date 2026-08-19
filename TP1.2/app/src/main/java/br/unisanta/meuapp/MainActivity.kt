f9e5bbabc9ba445f82c37906ead853ea85630a39
Lucas Barros Simon <lucasweacked21@gmail.com>
2026-08-19 12:39:28 -0300

    feat: terminei o tp1.2

--- TP1.2/app/src/main/java/br/unisanta/meuapp/MainActivity.kt
+++ TP1.2/app/src/main/java/br/unisanta/meuapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package br.unisanta.meuapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,20 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val edtCelsius = findViewById<EditText>(R.id.edt_celsius)
+        val btnConverter = findViewById<Button>(R.id.btn_converter)
+        val txvResultado = findViewById<TextView>(R.id.txv_resultado)
+
+        btnConverter.setOnClickListener {
+            val celsius = edtCelsius.text.toString().toDouble()
+
+            val fahrenheit = (celsius * 9 / 5) + 32
+
+            txvResultado.text = "Temperatura em Fahrenheit: $fahrenheit °F"
+
+            edtCelsius.text.clear()
         }
     }
 }
